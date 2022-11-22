cb252f63c7269bb76afe39f6f7220bc031002d16
2034254 <le2034254@crc-lennox.qc.ca>
2022-11-22 11:54:28 -0500

    Created, programmed and commented base features of the Room class.

---
+++ MovieTownFinalProject/Room.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTownFinalProject
+{
+    /// <summary>
+    /// A Room.
+    /// </summary>
+    internal class Room
+    {
+        /// <summary>
+        /// System's name for the room.
+        /// </summary>
+        public int RoomId { get; }
+        /// <summary>
+        /// Visible name for the room.
+        /// </summary>
+        public string RoomName { get; set; }
+        /// <summary>
+        /// Number of seats in the room.
+        /// </summary>
+        public int NumberOfSeats { get; set; }
+
+        /// <summary>
+        /// A Room.
+        /// </summary>
+        public Room()
+        {
+
+        }
+    }
+}
