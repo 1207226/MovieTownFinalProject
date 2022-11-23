3b7e641d53269cf8c210239373c5c89f2648f882
2034254 <le2034254@crc-lennox.qc.ca>
2022-11-23 09:53:34 -0500

    Created and programmed MovieTheatre class.

---
+++ MovieTownFinalProject/MovieTheatre.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="MovieTheatre.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MovieTownFinalProject
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A movie theatre.
+    /// </summary>
+    internal class MovieTheatre
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MovieTheatre"/> class.
+        /// </summary>
+        public MovieTheatre()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets list of rooms.
+        /// </summary>
+        public List<Room> Rooms { get; set; }
+
+        /// <summary>
+        /// Gets or sets list of users.
+        /// </summary>
+        public List<User> Users { get; set; }
+
+        /// <summary>
+        /// Gets or sets list of movies.
+        /// </summary>
+        public List<Movie> Movies { get; set; }
+    }
+}
