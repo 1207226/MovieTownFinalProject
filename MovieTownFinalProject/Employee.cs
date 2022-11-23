c690f36b395c25d3327102aab5c6a03ac08174de
2034254 <le2034254@crc-lennox.qc.ca>
2022-11-23 09:15:49 -0500

    Added Client and Employee classes and coded a part.

---
+++ MovieTownFinalProject/Employee.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Employee.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MovieTownFinalProject
+{
+    /// <summary>
+    /// An employee.
+    /// </summary>
+    internal class Employee : User
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Employee"/> class.
+        /// </summary>
+        public Employee()
+        {
+        }
+
+        /// <summary>
+        /// Gets the system's name for the employee.
+        /// </summary>
+        public int EmployeeId { get; }
+    }
+}
