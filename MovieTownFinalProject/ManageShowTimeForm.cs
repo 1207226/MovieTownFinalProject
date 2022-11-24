246d005dfe1c1555017b7b464385ce82ebffea8a
Christophe <LE1207226@crc.lennox.qc.ca>
2022-11-24 12:09:28 -0500

    Create ManageShowTimeForm

---
+++ MovieTownFinalProject/ManageShowTimeForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieTownFinalProject
+{
+    public partial class ManageShowTimeForm : Form
+    {
+        public ManageShowTimeForm()
+        {
+            InitializeComponent();
+        }
+                /// <summary>
+                /// Set the Size of the Back Arrow to the Size of the Button.
+                /// </summary>
+                /// <param name="sender"></param>
+                /// <param name="e"></param>
+        private void ManageShowTimeForm_Load(object sender, EventArgs e)
+        {
+            backButton.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+        /// <summary>
+        /// Opens Selection Menu Form Form and Closes the Show Time Manager Form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            ManagerSelectMenuForm SelectMenu = new ManagerSelectMenuForm();
+            this.Hide();
+            SelectMenu.ShowDialog();
+            this.Close();
+        }
+    }
+}
