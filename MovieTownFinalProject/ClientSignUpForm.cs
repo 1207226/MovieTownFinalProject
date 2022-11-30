a815d6d30db7a690099d555ba671808031fc9058
Christophe <LE1207226@crc.lennox.qc.ca>
2022-11-30 09:06:14 -0500

    Created ClientSignUpForm & it's elements.

---
+++ MovieTownFinalProject/ClientSignUpForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieTownFinalProject
+{
+    public partial class ClientSignUpForm : Form
+    {
+        public ClientSignUpForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
