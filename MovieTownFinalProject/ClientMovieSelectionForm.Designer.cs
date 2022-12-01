afd5601288bc9b260630757656876b112bae35cc
Christophe <LE1207226@crc.lennox.qc.ca>
2022-11-30 20:54:32 -0500

    Set basic form elements.

--- MovieTownFinalProject/ClientMovieSelectionForm.Designer.cs
+++ MovieTownFinalProject/ClientMovieSelectionForm.Designer.cs
@@ -28,12 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientMovieSelectionForm));
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.employeeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.signupButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo.Image")));
+            this.pictureBoxLogo.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo.InitialImage")));
+            this.pictureBoxLogo.Location = new System.Drawing.Point(116, 63);
+            this.pictureBoxLogo.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(501, 236);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLogo.TabIndex = 1;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(5)))), ((int)(((byte)(22)))), ((int)(((byte)(21)))));
+            this.welcomeLabel.Location = new System.Drawing.Point(312, 325);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(109, 25);
+            this.welcomeLabel.TabIndex = 10;
+            this.welcomeLabel.Text = "Welcome!";
+            // 
+            // employeeButton
+            // 
+            this.employeeButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.employeeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.employeeButton.FlatAppearance.BorderSize = 0;
+            this.employeeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.employeeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeButton.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.employeeButton.Location = new System.Drawing.Point(302, 766);
+            this.employeeButton.Margin = new System.Windows.Forms.Padding(4);
+            this.employeeButton.Name = "employeeButton";
+            this.employeeButton.Size = new System.Drawing.Size(127, 38);
+            this.employeeButton.TabIndex = 11;
+            this.employeeButton.Text = "Employee";
+            this.employeeButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(5)))), ((int)(((byte)(22)))), ((int)(((byte)(21)))));
+            this.label1.Location = new System.Drawing.Point(311, 627);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Welcome!";
+            // 
+            // signupButton
+            // 
+            this.signupButton.BackColor = System.Drawing.Color.MidnightBlue;
+            this.signupButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.signupButton.FlatAppearance.BorderSize = 0;
+            this.signupButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signupButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signupButton.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.signupButton.Location = new System.Drawing.Point(301, 690);
+            this.signupButton.Margin = new System.Windows.Forms.Padding(4);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Size = new System.Drawing.Size(129, 38);
+            this.signupButton.TabIndex = 13;
+            this.signupButton.Text = "Sign-Up";
+            this.signupButton.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(161, 622);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 38);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Sign-Up";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Location = new System.Drawing.Point(438, 622);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 38);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Sign-Up";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(5)))), ((int)(((byte)(22)))), ((int)(((byte)(21)))));
+            this.label2.Location = new System.Drawing.Point(312, 583);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Welcome!";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(116, 372);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(116, 411);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(501, 154);
+            this.panel1.TabIndex = 18;
+            // 
+            // ClientMovieSelectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(152)))), ((int)(((byte)(158)))));
+            this.ClientSize = new System.Drawing.Size(732, 846);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.signupButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.employeeButton);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Name = "ClientMovieSelectionForm";
             this.Text = "ClientMovieSelectionForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Button employeeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button signupButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
