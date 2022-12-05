31c400ccd7720ebcb047e41ba71afaa9b102d490
2034254 <le2034254@crc-lennox.qc.ca>
2022-12-05 09:59:54 -0500

    Created the Showtime class.

---
+++ MovieTownFinalProject/Showtime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MovieTownFinalProject
+{
+    internal class Showtime
+    {
+        public Showtime(Movie movie, Room room, DateTime dateTime)
+        {
+            ShowtimeMovie = movie;
+            ShowtimeRoom = room;
+            ShowtimeDate = dateTime;
+        }
+
+        public int ShowtimeId { get; set; }
+
+        public Movie ShowtimeMovie { get; set; }
+
+        public Room ShowtimeRoom { get; set; }
+
+        public DateTime ShowtimeDate { get; set; }
+    }
+}
